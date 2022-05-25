7021384e2067153de8b779e1a27ff94b36324abc
Ibtesham Akhtar <55596635+Ibtesham42@users.noreply.github.com>
2022-05-25 14:29:39 +0530

    Add files via upload

--- assp4.c
+++ assp4.c
@@ -1,33 +1,41 @@
 //wap Array Deletion
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-	int a[50],i,size,pos,item;
-	printf("Enter Size of the elements ");
-	scanf("%d",&size);
-	printf("Enter Elements ");
-	for(i=0;i<size;i++)
-	{
-	scanf("%d",a[i]);
-	}
-	printf("Enter Position ");
-	scanf("%d",&pos);
-	if(pos<0||pos>size)
-	{
-	printf("\nInvalid Position ");
-	}
-	else
-	{
-	item=a[pos-1];
-	for(i=pos-1;i<size-1;i++)
-	{
-	a[i]=a[i+1];
-	}
-	size--;
-	printf("\nAfter Deletion Elements\n");
-	for(i=0;i<size;i++)
-	{
-	printf("%d\t",a[i]);
-	}
-	}
+    int i,n,a[50],pos,item;
+    printf("Enter The Array Size\n");
+    scanf("%d",&n);
+    printf("Enter Elements \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+    printf("your elements are \n");
+    for(i=0;i<n;i++)
+        {
+            printf("%d\t",a[i]);
+        }
+    printf("\nEnter the position you want to delelte data\n");
+    scanf("%d",&pos);
+    if(pos<=0 || pos>n)
+    {
+        printf("Invalid position");
+    }
+    else{
+        item=a[pos-1];
+        for(i=pos-1;i<n-1;i++)
+        {
+            a[i]=a[i+1];
+        }
+        n--;
+    }
+    printf("\nUpdated array after deletion\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    printf("\nYour Deleted item is %d ",item);
 }
