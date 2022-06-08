fbcbb394da80bd4b9c44b78853d035d0d1913f45
Ibtesham Akhtar <55596635+Ibtesham42@users.noreply.github.com>
2022-06-08 15:05:27 +0530

    Add files via upload

---
+++ p.s7.c
@@ -0,0 +1,39 @@
+//implementatin of singly linklist//
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+struct node
+{
+	struct node *next;
+	int data;
+};
+struct node *head,*newnode,*temp;
+head=0;
+int choice;
+while(choice)
+{
+	newnode=(struct node *)malloc(sizeof(struct node));
+	printf("Enter the data:\n");
+	scanf("%d",&newnode->data);
+	newnode->next=0;
+	if(head==0)
+	{
+		head=temp=newnode;
+	}
+	else
+	{
+		temp->next=newnode;
+		temp=newnode;
+	}
+	printf("Do you want to continue(0,1)?");
+	scanf("%d",&choice);
+}
+temp=head;
+while(temp!=0)
+{
+		printf("%d\t",temp->data);
+		temp=temp->next;
+}
+}
+
